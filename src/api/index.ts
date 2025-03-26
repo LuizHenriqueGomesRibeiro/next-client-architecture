43127e21e8dfc188d81d653962236cd083767f0d
Luiz Gomes <louisnuuk@hotmail.com>
2025-03-26 20:39:59 -0300

    feat

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,3 +1,11 @@
+interface ApiEndpoint<ArgsProps = unknown, DataProps = unknown> {
+    readonly url: string;
+    readonly method: "get" | "post" | "put" | "delete";
+    readonly authenticated: boolean;
+    readonly ARGS_PROPS?: ArgsProps;
+    readonly DATA_PROPS?: DataProps;
+}
+
 interface BreedsImageRandomArgProps {
     breed?: string,
 }
@@ -6,21 +14,6 @@ interface BreedsImageRandomDataProps {
     message: string,
     status: string,
 }
-
-interface EndpointBase {
-    url: string;
-    method: 'get' | 'post' | 'put' | 'delete';
-    authenticated: boolean;
-}
-
-type Endpoint<ArgsProps = unknown, DataProps = unknown> = EndpointBase & {
-    ARGS_PROPS?: ArgsProps;
-    DATA_PROPS?: DataProps;
-};
-
-type Endpoints = {
-    [key: string]: Endpoint;
-};
 
 export const api = {
     breeds_image_random: { 
@@ -32,4 +25,4 @@ export const api = {
     },
     breeds_list_all: { url: '/breeds/list/all', method: 'get', authenticated: false, },
     breed_hound_images: { url: '/breed/hound/images', method: 'get', authenticated: false, },
-}
+} as const satisfies Record<string, ApiEndpoint>;
