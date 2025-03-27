465b039238ad0bfd16b63d0f242927c05cb953ee
Luiz Gomes <louisnuuk@hotmail.com>
2025-03-27 11:30:21 -0300

    feat

--- src/api/index.ts
+++ src/api/index.ts
@@ -9,12 +9,12 @@ interface BreedsImageRandomDataProps {
     status: string,
 }
 
+interface BreedsHoudImagesDataProps {
+    message: string[],
+    status: string
+}
+
 export const api = {
-    login: {
-        url: '/login',
-        method: 'post',
-        authenticated: false,
-    },
     breeds_image_random: { 
         url: '/breeds/image/random', 
         method: 'get', 
@@ -22,6 +22,10 @@ export const api = {
         ARGS_PROPS: {} as BreedsImageRandomArgProps,  
         DATA_PROPS: {} as BreedsImageRandomDataProps,
     },
-    breeds_list_all: { url: '/breeds/list/all', method: 'get', authenticated: false, },
-    breed_hound_images: { url: '/breed/hound/images', method: 'get', authenticated: false, },
+    breed_hound_images: { 
+        url: '/breed/hound/images', 
+        method: 'get', 
+        authenticated: false, 
+        DATA_PROPS: {} as BreedsHoudImagesDataProps,
+    },
 } as const satisfies Record<string, ApiEndpoint>;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,3 @@
-import { client, server } from "@/services/api";
-
 export const getServerSideProps = async () => {
   return {
     props: {
@@ -9,19 +7,9 @@ export const getServerSideProps = async () => {
 }
 
 export default function Home() {
-  const { makeRequest, data, isLoading, isSuccess} = client.breeds_image_random();
 
-  const status = () => {
-    if (isLoading && !isSuccess) return 'Carregando...';
-    if (!isLoading && isSuccess) return 'Carregado com sucesso';
-    return 'Aguardando requisição';
-  }
 
   return <>
-    <button className="bg-red-400 rounded-2xl px-2 py-1 cursor-pointer" onClick={() => makeRequest()}>Nova requisição</button>
-    <div>Status da requisição: 
-      <span className="font-bold text-5xl">{status()}</span>
-    </div>
-    {data && <img src={data.message} alt="" />}
+
   </>
 }
