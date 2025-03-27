fea75f844cb34b7b160bc956f66ed20503dbf228
Luiz Gomes <louisnuuk@hotmail.com>
2025-03-27 18:51:45 -0300

    feat

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,33 +1,7 @@
 import { ApiEndpoint } from "@/services/endpoints/types";
 
-interface BreedsImageRandomArgProps {
-    breed?: string,
-}
-
-interface BreedsImageRandomDataProps {
-    message: string,
-    status: string,
-}
-
-interface BreedsHoudImagesDataProps {
-    message: string[],
-    status: string
-}
-
-export const BASE_URL = "https://dog.ceo/api";
+export const BASE_URL = "";
 
 export const api = {
-    breeds_image_random: { 
-        url: '/breeds/image/random', 
-        method: 'get', 
-        authenticated: false, 
-        ARGS_PROPS: {} as BreedsImageRandomArgProps,  
-        DATA_PROPS: {} as BreedsImageRandomDataProps,
-    },
-    breed_hound_images: { 
-        url: '/breed/hound/images', 
-        method: 'get', 
-        authenticated: false, 
-        DATA_PROPS: {} as BreedsHoudImagesDataProps,
-    },
+    
 } as const satisfies Record<string, ApiEndpoint>;
