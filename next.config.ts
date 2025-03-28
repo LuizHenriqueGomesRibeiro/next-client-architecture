cf35568c2ecfcb6934457da49b57fde55a7c7211
Luiz Gomes <louisnuuk@hotmail.com>
2025-03-28 14:55:38 -0300

    feat

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,19 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   webpack(config) {
+    config.module.rules.push({
+      test: /\.tsx?$/,
+      use: [
+        {
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: true,
+          },
+        },
+      ],
+      exclude: /node_modules/,
+    });
+
     return config;
   },
 };
