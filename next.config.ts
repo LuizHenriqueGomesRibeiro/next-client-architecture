6423bec62dc5d5225e4dfc0c54047788b99d7e63
Luiz Gomes <louisnuuk@hotmail.com>
2025-03-28 14:48:40 -0300

    feat

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  webpack(config, { isServer }) {
-    config.module.rules.push({
-      test: /\.tsx?$/,
-      use: 'ts-loader',
-      exclude: /node_modules/,
-    });
-    
+  webpack(config) {
     return config;
   },
 };
