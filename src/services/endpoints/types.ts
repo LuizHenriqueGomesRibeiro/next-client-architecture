cf286449d86a174fcac26616df248a81b4cc86a0
Luiz Gomes <louisnuuk@hotmail.com>
2025-03-29 19:33:37 -0300

    feat

--- src/services/endpoints/types.ts
+++
@@ -1,9 +0,0 @@
-import { MethodProps } from "../api/types";
-
-export interface ApiEndpoint<ArgsProps = unknown, DataProps = unknown> {
-    readonly url: string;
-    readonly method: MethodProps;
-    readonly authenticated: boolean;
-    readonly ARGS_PROPS?: ArgsProps;
-    readonly DATA_PROPS?: DataProps;
-}
