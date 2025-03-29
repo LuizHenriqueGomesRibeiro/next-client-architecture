3142c795afc7151f4620259e00a6fd16246f5d98
Luiz Gomes <louisnuuk@hotmail.com>
2025-03-29 19:10:35 -0300

    feat

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
 import { ApiClientInstanceType, PrimitiveClient } from "./services/api/client";
 import { PrimitiveServer, ServerInstanceType } from "./services/api/server";
+import { MethodProps } from "./services/api/types";
+
+export interface ApiEndpoint<ArgsProps = unknown, DataProps = unknown> {
+    readonly url: string;
+    readonly method: MethodProps;
+    readonly authenticated: boolean;
+    readonly ARGS_PROPS?: ArgsProps;
+    readonly DATA_PROPS?: DataProps;
+}
 
 //@ts-ignore
 const serverNextClientArchitecture: ServerInstanceType = new PrimitiveServer();
