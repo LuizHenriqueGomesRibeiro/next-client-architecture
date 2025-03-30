2db228f77cf69e3ff85b9d1ce7b567013eff7be6
Luiz Gomes <louisnuuk@hotmail.com>
2025-03-29 21:02:51 -0300

    feat

---
+++ createFile.js
@@ -0,0 +1,35 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const projectRoot = path.resolve(__dirname, '..', '..', '..');
+const dirPath = path.join(projectRoot, 'src', 'api');
+const filePath = path.join(dirPath, 'index.ts');
+
+const content = `
+import { ApiEndpoint } from "@caucolum/next-client-architecture";
+
+export const BASE_URL = "https://dog.ceo/api";
+
+export const api = {
+    breeds_image_random: {
+        url: "breeds/image/random",
+        authenticated: false,
+        method: 'get'
+    },
+    breed_hound_images: {
+        url: "breed/hound/images",
+        authenticated: false,
+        method: 'get'
+    },
+} as const satisfies Record<string, ApiEndpoint>;
+`;
+
+if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath, { recursive: true });
+}
+
+fs.writeFileSync(filePath, content.trim() + '\n', 'utf8');
