905eee9a705ced5a181a6e6db7774b4106e73965
Luiz Gomes <louisnuuk@hotmail.com>
2025-04-01 08:11:32 -0300

    feat

--- src/api/types.ts
+++ src/api/types.ts
@@ -10,6 +10,21 @@ export type ApiConfig = {
     };
 };
 
+interface ApiClientResourcesProps<T = any, K = any> {
+    makeRequest: (props?: K) => void,
+    data: T,
+    args: K,
+    isLoading: boolean,
+    isSuccess: boolean, 
+    isPaused: boolean,
+    isError: boolean,
+    isIdle: boolean,
+}
+
 export type ServerApiMethods<T extends ApiConfig> = {
     [K in keyof T]: (params?: T[K]['ARGS_PROPS']) => Promise<T[K]['DATA_PROPS']>;
+};
+
+export type ClientApiMethods<T extends ApiConfig> = {
+    [K in keyof T]: (params?: any) => ApiClientResourcesProps<T[K]["DATA_PROPS"], T[K]["ARGS_PROPS"]>;
 };
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { ApiConfig, MethodProps, ServerApiMethods } from "./api/types";
+import { ApiConfig, ClientApiMethods, MethodProps, ServerApiMethods } from "./api/types";
 import http from "./http";
 
 export interface ApiEndpoint<ArgsProps = unknown, DataProps = unknown> {
@@ -8,12 +8,6 @@ export interface ApiEndpoint<ArgsProps = unknown, DataProps = unknown> {
     readonly ARGS_PROPS?: ArgsProps;
     readonly DATA_PROPS?: DataProps;
 }
-
-export const BASE_URL = "";
-
-export const api = {
-    
-} as const satisfies Record<string, ApiEndpoint>;
 
 function createApiClass<T extends ApiConfig>(list: T) {
     return class Api {
@@ -33,6 +27,21 @@ function createApiClass<T extends ApiConfig>(list: T) {
     };
 }
 
+function createPrimitiveClient<T extends ServerApiMethods<any>>(serverApi: T): new () => { [K in keyof T]: () => any } {
+    class PrimitiveClient {
+        constructor() {
+            Object.keys(serverApi).forEach((key) => {
+                (this as any)[key] = () => {
+                    //@ts-ignore
+                    return useServiceCall({ fn: serverApi[key as keyof T] }) as ApiClientResourcesProps; 
+                };
+            });
+        }
+    }
+
+    return PrimitiveClient as new () => { [K in keyof T]: () => any };
+}
+
 function createServerNextArchitecture<T extends ApiConfig>(list: T) {
     const PrimitiveServer = createApiClass(list);
     //@ts-ignore
@@ -40,6 +49,13 @@ function createServerNextArchitecture<T extends ApiConfig>(list: T) {
     return server;
 }
 
+function createClientNextArchitecture<T extends ServerApiMethods<any>, K extends ApiConfig>(serverApi: T, list: K) {
+    const PrimitiveClient = createPrimitiveClient(serverApi);
+    const client: ClientApiMethods<typeof list> = new PrimitiveClient();
+    return client;
+}
+
 export {
-    createServerNextArchitecture
+    createServerNextArchitecture,
+    createClientNextArchitecture
 }
